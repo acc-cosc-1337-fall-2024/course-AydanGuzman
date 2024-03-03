7abd81f12855f07dcecfb6bfa3e0f480a4792f7d
AydanGuzman <157077972+AydanGuzman@users.noreply.github.com>
2024-03-03 04:24:28 +0000

    Homework 5 Solution

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,60 @@
 //add include statements
+#include "func.h"
+#include <string>
 
 //add function code here
+double get_gc_content(const std::string& dna) 
+{
+    double count_gc = 0;
+    double gc_content = 0;
+
+    for (char get_gc : dna)
+    {
+        if (get_gc == 'C' || get_gc == 'G')
+        {
+            count_gc++;
+        }
+    }
+
+    gc_content = (100 / dna.length()) * count_gc;
+    return gc_content;
+
+}
+
+std::string get_reverse_string(const std::string dna) 
+{
+    std::string reversed_dna = "";
+    for (int index = dna.length() - 1 ; index >= 0; index--)
+    {
+        reversed_dna += dna[index];
+    }
+
+    return reversed_dna;
+}
+
+std::string get_dna_complement(const std::string dna) 
+{
+    std::string complement_dna;
+    for (char complement : dna) 
+    {
+        switch (complement)
+        {
+            case 'A':
+                complement_dna += 'T';
+                break;
+            case 'T':
+                complement_dna += 'A';
+                break;
+            case 'C':
+                complement_dna += 'G';
+                break;
+            case 'G':
+                complement_dna += 'C';
+                break;
+            default:
+                complement_dna += complement;
+        }
+        
+    }
+    return complement_dna;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,50 @@
+#include <iostream>
+#include <string>
+#include "func.h"
+
+using std::cout; using std::string;
 
 int main() 
 {
+	int choice;
+	std::string dna;
 
+	do {
+		std::cout<<"Menu \n";
+		std::cout<<"\n";
+		std::cout<<"1 - Get GC Content \n";
+		std::cout<<"2 - Get DNA Complement \n";
+		std::cout<<"3 - Exit \n";
+		std::cout<<"Enter your choice:  ";
+		std::cin >> choice;
+		std::string dna_result;
+
+		switch (choice) 
+		{
+			
+			case 1:
+				std::cout << "Enter DNA string: ";
+				std::cin >> dna;
+				std::cout << "GC Content " << get_gc_content(dna) << "% \n";
+				break;
+			case 2:
+				
+				std::cout << "Enter DNA string: ";
+				std::cin >> dna;
+				dna_result = get_dna_complement(dna);
+				std::cout << "DNA Complement: " << get_reverse_string(dna_result) << "\n";
+				
+				break;
+			case 3: 
+				std::cout << "Exiting the Program. \n";
+				break;
+			default:
+				std::cout << "Invalid choice. \n";
+
+
+		}
+
+	}	while (choice != 3);
+	
 	return 0;
 }
